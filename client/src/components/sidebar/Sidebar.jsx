568892ce479904344b2c070da5703e76edf89eb1
Maheshwaran <maheshwaran325@gmail.com>
2021-12-26 17:43:32 +0530

    first commit

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []); 
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img className="about_img"
+          src="https://t4.ftcdn.net/jpg/02/14/74/61/360_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg"
+          alt=""/>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui
+          necessitatibus nostrum illum reprehenderit.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+            <li className="sidebarListItem">{c.name}</li>
+            </Link> 
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
